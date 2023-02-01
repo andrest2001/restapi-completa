1d81968ff1ffa209f1f86642b1803a0fba1ac2ac
Andrés Toledano <andres13072001@gmail.com>
2023-02-01 13:58:14 +0100

    Rutas funcionales al inicio

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -12,9 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use(express.static(path.join(__dirname, 'public')));
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) =>{
+  res.send('Hola mundo');
 });
 
+router.get('/employees', (req, res, next) =>{
+  res.send('Obteniendo empleados');
+});
+
+router.post('/employees', (req, res, next) =>{
+  res.send('Creando empleados');
+});
+
+router.put('/employees', (req, res, next) =>{
+  res.send('Actualizando empleados');
+});
+
+router.delete('/employees', (req, res, next) =>{
+  res.send('Eliminando empleados');
+});
 module.exports = router;
