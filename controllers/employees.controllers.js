75660172e7dce78139c16eda045ffb2f3f87a254
Andrés Toledano <andres13072001@gmail.com>
2023-02-03 10:17:30 +0100

    Terminado el metodo get que te printea la base de datos completa

--- controllers/employees.controllers.js
+++ controllers/employees.controllers.js
@@ -1,13 +1,19 @@
+const { query } = require('../db.js');
 const pool=require('../db.js')
 
-const getEmployees = (req, res, next) =>{
-    res.send('Obteniendo empleados');
+const getEmployees = async(req, res, next) =>{
+    const[row]=await pool.query('SELECT * FROM employee')
+    res.json(row);
 }
 const createEmployees = async(req, res, next) =>{
     const{name, salary}= req.body
     const [rows]=await pool.query('INSERT INTO employee (name, salary) VALUES (?,?)', [name, salary])
     //console.log(name, salary)
-    res.send({rows});
+    res.send({
+        id: rows.insertId,
+        name,
+        salary
+    });
 }
 
 const updateEmployees = (req, res, next) =>{
