f9aea2e80a9570ac4d96db870c01204417339838
Andrés Toledano <andres13072001@gmail.com>
2023-02-07 10:19:08 +0100

    Controlador de errores

--- routes/employees.js
+++ routes/employees.js
@@ -9,7 +9,7 @@ router.get('/employees/:id', getEmployee);
   
 router.post('/employees', createEmployee);
   
-router.put('/employees/:id', updateEmployee);
+router.patch('/employees/:id', updateEmployee);
   
 router.delete('/employees/:id', deleteEmployee);
 
